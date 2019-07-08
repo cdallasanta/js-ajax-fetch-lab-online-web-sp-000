622bf5e4584094d8305b6e0eb8a4ed9f408e5448
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-08 20:51:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+<<<<<<< HEAD
 const baseURL = 'https://api.github.com/repos';
 const repo = 'js-ajax-fetch-lab';
 const learnUser = 'learn-co-curriculum';
+=======
+const baseURL = 'https://api.github.com';
+>>>>>>> e8c2d34609089a8883806536deb56ab26c7f2788
 const user = 'cdallasanta';
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
+  const token = 'b3a0bf87389d199613939033d863e21e84ea09f0';
   return '';
 }
 
 function forkRepo() {
+<<<<<<< HEAD
   fetch (baseURL + '/' + learnUser + '/' + repo + '/forks', {
     method: "POST",
     headers: {
@@ -18,6 +24,17 @@ function forkRepo() {
   })
     .then(response => response.json())
     .then(data => showResults(data))
+=======
+  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  fetch (repo, {
+    method: "POST",
+    headers: {
+      Authentication: `token ${}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => showResults(data));
+>>>>>>> e8c2d34609089a8883806536deb56ab26c7f2788
 }
 
 function showResults(json) {
